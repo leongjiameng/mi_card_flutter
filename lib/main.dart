d4332838a995c6dfc9cdbe92d01edf36ba09d764
Leong Jia Meng <leongjiameng@outlook.com>
2022-04-11 15:33:03 +0800

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                color: Colors.red,
+                height: 100,
+                width: 100,
+                child: Text('Hello'),
+              ),
+              SizedBox(
+                width: 55,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+              SizedBox(
+                width: 55,
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100,
+                child: Text('Bye'),
+              ),
+            ],
+          )
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
